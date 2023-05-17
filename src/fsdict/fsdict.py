a66af15f8c52293248136502d2601ff1866fddce
MNayer <marie.nayer@web.de>
2023-05-17 11:48:54 +0200

    Add select feature

--- src/fsdict/fsdict.py
+++ src/fsdict/fsdict.py
@@ -1,6 +1,21 @@
 import json
+import types
 from fsdict.utils import *
 from pathlib import Path
+
+
+class LazyValue:
+    def __init__(self, path):
+        self.path = path
+
+    def __repr__(self):
+        return f"<LazyValue @ {self.path}>"
+
+    def __str__(self):
+        return repr(self)
+
+    def read(self):
+        return maybe_deserialize(fread_bytes(self.path))
 
 
 class fsdict:
@@ -12,19 +27,49 @@ class fsdict:
                 self.path.mkdir()
             assert self.path.is_dir()
 
-    def __getitem__(self, key):
+    def __getitem__(self, selector):
         assert not self.dangling()
-        if not self.has_key(key):
-            raise KeyError(key)
-        key_path = self.__get_keypath(key)
+        if isinstance(selector, str):
+            return self.__get_item(selector)
+        elif isinstance(selector, types.FunctionType):
+            return self.__get_items(selector)
+
+    def __setitem__(self, selector, value):
+        assert not self.dangling()
+        if isinstance(selector, str):
+            self.__set_item(selector, value)
+        elif isinstance(selector, types.FunctionType):
+            self.__set_items(selector, value)
+
+    def __delitem__(self, selector):
+        assert not self.dangling()
+        if isinstance(selector, str):
+            self.__del_item(selector)
+        elif isinstance(selector, types.FunctionType):
+            self.__del_items(selector)
+
+    def __repr__(self):
+        return json.dumps(self.todict(), indent=2, default=repr)
+
+    def __get_item(self, key):
+        assert not self.dangling()
+        assert isinstance(key, str)
+        key_path = self.__get_path(key)
+        if not key_path.exists():
+            raise KeyError(key_path.name)
         if self.__is_fsdict(key):
             return fsdict(key_path)
         else:
             return maybe_deserialize(fread_bytes(key_path))
 
-    def __setitem__(self, key, value):
+    def __get_items(self, selector):
         assert not self.dangling()
-        key_path = self.__get_keypath(key)
+        assert isinstance(selector, types.FunctionType)
+        keys = filter(selector, self.keys())
+        yield from (self[key] for key in keys)
+
+    def __set_item(self, key, value):
+        key_path = self.__get_path(key)
         if key_path.exists():
             if not self.overwrite:
                 return
@@ -37,14 +82,25 @@ class fsdict:
         else:
             fwrite_bytes(key_path, maybe_serialize(value))
 
-    def __delitem__(self, key):
+    def __set_items(self, selector, value):
         assert not self.dangling()
-        key_path = self.__get_keypath(key)
+        assert isinstance(selector, types.FunctionType)
+        keys = filter(selector, self.keys())
+        for key in keys:
+            self[key] = value
+
+    def __del_item(self, key):
+        assert not self.dangling()
+        key_path = self.__get_path(key)
         if key_path.exists():
             rm(key_path)
 
-    def __repr__(self):
-        return json.dumps(self.todict(), indent=2)
+    def __del_items(self, selector):
+        assert not self.dangling()
+        assert isinstance(selector, types.FunctionType)
+        keys = filter(selector, self.keys())
+        for key in keys:
+            del self[key]
 
     def dangling(self):
         return self.path == None
@@ -56,24 +112,24 @@ class fsdict:
         assert not self.dangling()
         dictionary = dict()
         for key in self.keys():
+            key_path = self.__get_path(key)
             if self.__is_fsdict(key):
-                key_path = self.__get_keypath(key)
                 dictionary[key] = fsdict(key_path).todict(lazy)
                 continue
             if lazy:
-                dictionary[key] = "<lazy>"
+                dictionary[key] = LazyValue(key_path)
             else:
                 dictionary[key] = self[key]
         return dictionary
 
     def has_key(self, key):
         assert not self.dangling()
-        key_path = self.__get_keypath(key)
+        key_path = self.__get_path(key)
         return key_path.exists()
 
     def keys(self, lazy=False):
         assert not self.dangling()
-        keys = (keypath.name for keypath in self.__get_keypaths())
+        keys = (keypath.name for keypath in self.__get_paths())
         if lazy:
             return keys
         else:
@@ -96,13 +152,13 @@ class fsdict:
         assert not self.dangling()
         symlink(self.path, dst_path)
 
-    def __get_keypath(self, key):
+    def __get_path(self, key):
         assert not self.dangling()
-        if not isinstance(key, str):
-            raise TypeError(f"Value of key must be of type 'str' not '{type(key)}'")
-        return self.path / key
+        if isinstance(key, str):
+            return self.path / key
+        raise TypeError(f"Value of key '{key}' must be of type 'str' not '{type(key)}'")
 
-    def __get_keypaths(self):
+    def __get_paths(self):
         assert not self.dangling()
         return self.path.glob("*")
 
